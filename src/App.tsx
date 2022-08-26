6f8a8d3eb72c16095f68e0c56faadd682e214c2d
juravlevdima <juravlevdima96@gmail.com>
2022-08-26 19:24:21 +0600

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react'
+import AppRoutes from './AppRoutes'
 
 const App: FC = () => {
   return (
-    <div>
-      App
-    </div>
+    <>
+      <AppRoutes />
+    </>
   )
 }
 
